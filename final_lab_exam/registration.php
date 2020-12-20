aa31a7143a33a359b6aa81df71186a92498d00c2
shaikhsadiq <sadiqshaikh807@gmail.com>
2020-12-20 11:39:35 +0600

    commit at 11:35

---
+++ final_lab_exam/registration.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration</title>
+</head>
+<body>
+    
+    <br><br><br><br><br><br>
+    
+    <center>
+        
+        <fieldset style="width:500px;">
+            
+            <legend><h1>Registration</h1></legend>
+            
+            <form>
+                
+                Id <br>
+                <input type="text" name="id"><br>
+                Password <br>
+                <input type="password" name="id"><br>
+                Confirm password <br>
+                <input type="password" name="id"><br>
+                Name <br>
+                <input type="text" name="id"><br>
+                Email <br>
+                <input type="email" name="id"><br>
+                
+                User Type <br>
+                
+                <select name="bloodGroup" >
+				    <option value="User">User</option>
+				    
+				
+			    </select>
+               
+               <hr>
+               
+               <input type="submit" name="submit" value="Register"> <a href="index.php">Login</a>
+                
+            </form>
+            
+        </fieldset>
+        
+    </center>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
